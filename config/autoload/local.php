2357a3b75bd8b6eef2a09ab3f18642e666c1cbe6
Shreemita Tuladhar <stuladhar@rocketlawyer.com>
2015-09-07 20:17:42 -0700

    git ignore

---
+++ config/autoload/local.php
@@ -0,0 +1,111 @@
+<?php
+
+return array(
+    'menuItem' => array(     
+        '0' => array(
+                'id'      	=> 1,
+                'name'      	=> 'Kale Pakoras',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Spiced crispy Kale served with fresh mint yogurt sause"
+            ),
+        '1' => array(
+                'id'      	=> 2,
+                'name'      	=> 'Classic Samosa',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Hand crafted Indian pastry filled with potatoes, peas, ginger,garlic and spices, serve with tamarind ginger sause and cilantro peanut sause"
+            ),
+       '2' => array(
+                'id'      	=> 3,
+                'name'      	=> 'Masala Croquettes',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 4.99,
+                'description'  	=> "Potatoes, mushroom and scallions mixe with roasted cumin and coriander, served with tomato chutney"
+            ),
+        '3' => array(
+                'id'      	=> 4,
+                'name'      	=> 'Kale Pakoras',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Spiced crispy Kale served with fresh mint yogurt sause"
+        ),
+        '4' => array(
+                'id'      	=> 5,
+                'name'      	=> 'Classic Samosa',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Hand crafted Indian pastry filled with potatoes, peas, ginger,garlic and spices, serve with tamarind ginger sause and cilantro peanut sause"
+        ),
+        '5' => array(
+                'id'      	=> 6,
+                'name'      	=> 'Masala Croquettes',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 4.99,
+                'description'  	=> "Potatoes, mushroom and scallions mixe with roasted cumin and coriander, served with tomato chutney"
+        ),
+        '6' => array(
+                'id'      	=> 7,
+                'name'      	=> 'Kale Pakoras',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Spiced crispy Kale served with fresh mint yogurt sause"
+            ),
+        '7' => array(
+                'id'      	=> 8,
+                'name'      	=> 'Classic Samosa',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Hand crafted Indian pastry filled with potatoes, peas, ginger,garlic and spices, serve with tamarind ginger sause and cilantro peanut sause"
+            ),
+       '8' => array(
+                'id'      	=> 9,
+                'name'      	=> 'Masala Croquettes',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 4.99,
+                'description'  	=> "Potatoes, mushroom and scallions mixe with roasted cumin and coriander, served with tomato chutney"
+            ), 
+        '9' => array(
+                'id'      	=> 10,
+                'name'      	=> 'Kale Pakoras',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Spiced crispy Kale served with fresh mint yogurt sause"
+            ),
+        '10' => array(
+                'id'      	=> 11,
+                'name'      	=> 'Classic Samosa',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Hand crafted Indian pastry filled with potatoes, peas, ginger,garlic and spices, serve with tamarind ginger sause and cilantro peanut sause"
+            ),
+       '11' => array(
+                'id'      	=> 12,
+                'name'      	=> 'Masala Croquettes',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 4.99,
+                'description'  	=> "Potatoes, mushroom and scallions mixe with roasted cumin and coriander, served with tomato chutney"
+            ), 
+        '12' => array(
+                'id'      	=> 13,
+                'name'      	=> 'Kale Pakoras',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Spiced crispy Kale served with fresh mint yogurt sause"
+            ),
+        '13' => array(
+                'id'      	=> 14,
+                'name'      	=> 'Classic Samosa',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 3.99,
+                'description'  	=> "Hand crafted Indian pastry filled with potatoes, peas, ginger,garlic and spices, serve with tamarind ginger sause and cilantro peanut sause"
+            ),
+       '14' => array(
+                'id'      	=> 15,
+                'name'      	=> 'Masala Croquettes',
+                'tags'    	=> array('appetizer'),
+                'price'       	=> 4.99,
+                'description'  	=> "Potatoes, mushroom and scallions mixe with roasted cumin and coriander, served with tomato chutney"
+            ),
+    ),
+);
